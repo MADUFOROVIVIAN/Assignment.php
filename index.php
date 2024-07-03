abdf9c37dbecd592db8eb21d959f92432cda89f7
MADUFOROVIVIAN <vivianmaduforo85@gmail.com>
2024-07-03 16:09:43 +0100

    Assignmet

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- <?php 
+
+// $greet = "Hello" ;
+// $name = "Daisy" ;
+// $course = "Backend";
+// $score = 70;
+// echo "$greet $name, your score in $course is $score";
+
+?> -->
+
+// Assignments
+
+<!-- <?php
+$intro = "Welcome";
+$name = "Daisy!";
+$question = "How may I help you today?";
+$reminder = "Jesus Loves You.";
+echo "$intro $name, $question, $reminder";
+?> -->
+
+<?php
+// define("name", "Tobi \n");
+// echo name;
+?>
+<?php include 'array.php'?>
